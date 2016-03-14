3e559936b5c6534d972628db917ddf3707ffe4d1
Mingqian Wang <mingqianwang96@gmail.com>
2016-03-14 00:36:41 +0000

    added @currentUsers commanand

--- server.js
+++ server.js
@@ -8,14 +8,34 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
-var clientInfo = {
+var clientInfo = {};
 
-};
+function sendCurrentUsers(socket) {
+  var info = clientInfo[socket.id];
+  var users = [];
 
-io.on('connection', function (socket) {
+  if (typeof info === 'undefined') {
+    return;
+  }
+  Object.keys(clientInfo).forEach(function (socketId) {
+    var userInfo = clientInfo[socketId];
+
+    if(info.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current users: ' + users.join(', '),
+    timestamp: moment().valueOf()
+  })
+}
+
+io.on('connection', function(socket) {
   console.log('User connected via socket.io!');
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function() {
     var userData = clientInfo[socket.id];
     if (typeof userData !== 'undefined') {
       socket.leave(userData.room);
@@ -38,20 +58,24 @@ io.on('connection', function (socket) {
     });
   });
 
-  socket.on('message', function (message){
+  socket.on('message', function(message) {
     console.log('message received:' + message.text);
 
-    message.timestamp = moment().valueOf();
-    io.to(clientInfo[socket.id].room).emit('message', message);
+    if (message.text === '@currentUsers') {
+      sendCurrentUsers(socket);
+    } else {
+      message.timestamp = moment().valueOf();
+      io.to(clientInfo[socket.id].room).emit('message', message);
+    }
   });
 
   socket.emit('message', {
-    name:'System',
-    text:'Welcome to the chat application',
+    name: 'System',
+    text: 'Welcome to the chat application',
     timestamp: moment().valueOf()
   });
 });
 
-http.listen(PORT, function () {
+http.listen(PORT, function() {
   console.log('server started');
 });
